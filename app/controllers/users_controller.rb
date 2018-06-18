229f4bf0193b878acb6fb8616d3e8dac293d40b2
sabamiso11 <adachi.business.11@gmail.com>
2018-06-19 02:23:57 +0900

    承認済みの面接日程の表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
     before_action :authenticate_user!, only: :show
     before_action :sign_in_required, only: [:show]
     def index
-        @users = User.where.not(id: current_user.id)
+        @users = User.select("users.*, interviews.start_interview").joins("LEFT OUTER JOIN interviews ON users.id = interviews.user_id AND interviews.state = 1").where.not(id: current_user.id).includes(:interviews)
     end
   
     def show
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ApplicationRecord
   enum sex: { 男:1, 女:2}
 
   has_many :interviews
+  scope :approvaled_interview, -> { joins(:interviews).where('interviews.state = 1') }
 end
