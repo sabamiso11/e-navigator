25401c378497c9cf0829afed7b2fb14b290f7d85
sabamiso11 <adachi.business.11@gmail.com>
2018-06-19 11:44:48 +0900

    ログイン認証のバグ(/users)の修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
-    before_action :authenticate_user!, only: :show
-    before_action :sign_in_required, only: [:show]
+    before_action :authenticate_user!
+    before_action :sign_in_required
     def index
-        @users = User.select("users.*, interviews.start_interview").joins("LEFT OUTER JOIN interviews ON users.id = interviews.user_id AND interviews.state = 1").where.not(id: current_user.id).includes(:interviews)
+        @users = User.select("users.*, interviews.start_interview").joins("LEFT OUTER JOIN interviews ON users.id = interviews.user_id AND interviews.state = 1").where.not(id: current_user.id)
     end
   
     def show
