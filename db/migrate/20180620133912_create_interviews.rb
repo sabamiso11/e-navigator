bfb88155c56c334c970decc93b7d41caf7841a50
sabamiso11 <adachi.business.11@gmail.com>
2018-06-20 22:58:35 +0900

    rails db:migrate

--- db/migrate/20180620133912_create_interviews.rb
+++ db/migrate/20180620133912_create_interviews.rb
@@ -1,7 +1,10 @@
 class CreateInterviews < ActiveRecord::Migration[5.1]
   def change
     create_table :interviews do |t|
+      t.datetime :start_interview
+      t.integer :state, null: false, default: 0
       t.timestamps
+      t.references :interviews, :user, index: true
     end
   end
 end
