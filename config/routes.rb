cc1a0563cbc0fc447f390f1101883287fbe90f9d
sabamiso11 <adachi.business.11@gmail.com>
2018-06-20 23:30:45 +0900

    ユーザーページをpageからusersに切り替え

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'pages#index'
-
-  get 'pages/show'
-
-  as :user do
-    get 'devise/registrations#edit', :to => 'pages#show', :as => :user_root
+  devise_scope :user do
+    root :to => 'devise/sessions#new'
   end
 
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
