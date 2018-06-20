1ea86abaa78a519187bed87ba8c229f12001be4a
sabamiso11 <adachi.business.11@gmail.com>
2018-06-21 00:31:57 +0900

    Interviews#createの追加

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -8,4 +8,20 @@ class InterviewsController < ApplicationController
         @user = User.find(params[:user_id])
         @interviews = @user.interviews.new
     end
+
+    def create
+        @user = User.find(params[:user_id])
+        @interview = @user.interviews.new(start_interview_params)
+        if @interview.save
+          redirect_to :action => "index"
+        else
+          render 'new'
+        end
+    end
+
+    private
+    def start_interview_params
+      params.require(:interview).permit(:start_interview)
+    end
+
 end
