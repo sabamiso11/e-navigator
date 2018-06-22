e9fd95f2a9fc0e98075c4e29ef0c4548818bfd74
sabamiso11 <adachi.business.11@gmail.com>
2018-06-22 13:14:11 +0900

    'users/'からusers_pathへ変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!  
 
   def after_sign_in_path_for(resource)
-    '/users'
+    users_path
   end
 
   protected
