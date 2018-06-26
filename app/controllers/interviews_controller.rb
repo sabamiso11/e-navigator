c6dc0b7633a8cf5e1f471a00dc0d72d6375dc9db
sabamiso11 <adachi.business.11@gmail.com>
2018-06-26 22:34:52 +0900

    スペース4からスペース2へ変更

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -1,67 +1,67 @@
 class InterviewsController < ApplicationController
-    def index
-        @user = User.find(params[:user_id])
-        @interviews = @user.interviews.all.order(start_interview: "ASC")
-        @interview = @user.interviews.new
-    end
+  def index
+    @user = User.find(params[:user_id])
+    @interviews = @user.interviews.all.order(start_interview: "ASC")
+    @interview = @user.interviews.new
+  end
 
-    def new
-        @user = User.find(params[:user_id])
-        @interview = @user.interviews.new
-    end
+  def new
+    @user = User.find(params[:user_id])
+    @interview = @user.interviews.new
+  end
 
-    def create
-        @user = User.find(params[:user_id])
-        @interview = @user.interviews.new(start_interview_params)
-        if @interview.save
-          redirect_to :action => "index"
-        else
-            #render plain: @interview.errors.inspect
-            render 'new'
-        end
+  def create
+    @user = User.find(params[:user_id])
+    @interview = @user.interviews.new(start_interview_params)
+    if @interview.save
+      redirect_to :action => "index"
+    else
+      #render plain: @interview.errors.inspect
+      render 'new'
     end
+  end
 
-    def edit
-        @user = User.find(params[:user_id])
-        @interview = @user.interviews.find(params[:id])
-    end
+  def edit
+    @user = User.find(params[:user_id])
+    @interview = @user.interviews.find(params[:id])
+  end
 
-    def update
-        @user = User.find(params[:user_id])
-        @interview = @user.interviews.find(params[:id])
-        if @interview.update(start_interview_params)
-          redirect_to :action => "index"
-        else
-          render 'edit'
-        end
+  def update
+    @user = User.find(params[:user_id])
+    @interview = @user.interviews.find(params[:id])
+    if @interview.update(start_interview_params)
+      redirect_to :action => "index"
+    else
+      render 'edit'
     end
+  end
 
-    def destroy
-        @user = User.find(params[:user_id])
-        @interview = @user.interviews.find(params[:id])
-        @interview.destroy
-        redirect_to :action => "index"
-    end
+  def destroy
+    @user = User.find(params[:user_id])
+    @interview = @user.interviews.find(params[:id])
+    @interview.destroy
+    redirect_to :action => "index"
+  end
 
-    def state
-        @user = User.find(params[:user_id])
-        @interviews = @user.interviews.all.order(start_interview: "ASC")
-        @interview = @user.interviews.find(params[:id])
-        @interview.state = 1
-        if @interview.save
-            dismissing_interview = @user.interviews.where(state: [0, 1]).where.not(id: @interview.id)
-            dismissing_interview.update_all(state: 2)
-            redirect_to :action => "index"
-        else
-            #render plain: @interviews.errors.inspect
-            #render :file => "_error.html.erb"
-            render 'index'
-        end
+  def state
+    @user = User.find(params[:user_id])
+    @interviews = @user.interviews.all.order(start_interview: "ASC")
+    @interview = @user.interviews.find(params[:id])
+    @interview.state = 1
+    if @interview.save
+      dismissing_interview = @user.interviews.where(state: [0, 1]).where.not(id: @interview.id)
+      dismissing_interview.update_all(state: 2)
+      redirect_to :action => "index"
+    else
+      #render plain: @interviews.errors.inspect
+      #render :file => "_error.html.erb"
+      render 'index'
     end
+  end
 
-    private
-    def start_interview_params
-      params.require(:interview).permit(:start_interview)
-    end
+  private
+  def start_interview_params
+    params.require(:interview).permit(:start_interview)
+  end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
-    before_action :authenticate_user!
-    before_action :sign_in_required
-    def index
-        @users = User.select("users.*, interviews.start_interview")
-          .joins("LEFT OUTER JOIN interviews ON users.id = interviews.user_id AND interviews.state = 1")
-          .where.not(id: current_user.id)
-    end
+  before_action :authenticate_user!
+  before_action :sign_in_required
+  def index
+    @users = User.select("users.*, interviews.start_interview")
+      .joins("LEFT OUTER JOIN interviews ON users.id = interviews.user_id AND interviews.state = 1")
+      .where.not(id: current_user.id)
+  end
 end
