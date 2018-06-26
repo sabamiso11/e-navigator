804b4e47d209ce52e8a8c643548dcf115cf8095a
sabamiso11 <adachi.business.11@gmail.com>
2018-06-27 02:08:02 +0900

    interviewer_approvalメソッドとexaminee_approvalメソッドの追加

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,2 +1,23 @@
 class NotificationMailer < ApplicationMailer
+  default from: 'test@gmail.com'
+
+  def interviewer_approval(interviewer, examinee)
+    @interviewer = interviewer
+    @examinee = examinee
+    @interview = @examinee.interviews.where(state: 1).first
+    mail(
+      to: @interviewer.email,
+      subject: '面接日程が決まりました。'
+    )
+  end
+
+  def examinee_approval(interviewer, examinee)
+    @interviewer = interviewer
+    @examinee = examinee
+    @interview = @examinee.interviews.where(state: 1).first
+    mail(
+      to: @examinee.email,
+      subject: '面接日程が決まりました。'
+    )
+  end
 end
