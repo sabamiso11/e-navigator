92c5514bd5af8399e98b8b1b9c13b30f273c02b4
sabamiso11 <adachi.business.11@gmail.com>
2018-06-26 23:52:38 +0900

    環境変数の設定

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -50,6 +50,14 @@ Rails.application.configure do
 
   # mailer setting
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address: ENV['address'],
+  port: ENV['port'],
+  domain: ENV['domain'],
+  user_name: ENV['user_name'],
+  password: ENV['password']
+}
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
