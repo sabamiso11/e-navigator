f497bb71ce1f667a3d6b6a584e9d9d791d9cad1f
sabamiso11 <adachi.business.11@gmail.com>
2018-06-27 03:28:43 +0900

    面接官と面接者への通知トリガーの追加(Users#desired)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,11 @@ class UsersController < ApplicationController
       .joins("LEFT OUTER JOIN interviews ON users.id = interviews.user_id AND interviews.state = 1")
       .where.not(id: current_user.id)
   end
+
+  def desired
+    @user = User.find(params[:user][:id])
+    NotificationMailer.interviewer_desired(@user, current_user).deliver_later
+    NotificationMailer.examinee_desired(current_user).deliver_later
+    redirect_to :action => "index"
+  end
 end
