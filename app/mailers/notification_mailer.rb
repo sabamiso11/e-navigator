1497d0552b21dc5477b4995456bab918f49fd130
sabamiso11 <adachi.business.11@gmail.com>
2018-06-27 03:24:13 +0900

    面接官用(interviewer)と面接者用(examinee)の面接日程希望メソッドを追加

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -20,4 +20,21 @@ class NotificationMailer < ApplicationMailer
       subject: '面接日程が決まりました。'
     )
   end
+
+  def interviewer_desired(interviewer, examinee)
+    @interviewer = interviewer
+    @examinee = examinee
+    mail(
+      to: @interviewer.email,
+      subject: '面接希望日が決定しました。'
+    )
+  end
+
+  def examinee_desired(examinee)
+    @examinee = examinee
+    mail(
+      to: @examinee.email,
+      subject: '面接希望日を登録しました。'
+    )
+  end
 end
