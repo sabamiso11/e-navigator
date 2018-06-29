2a1ac5864ff07a3fa42a5ef7051cb2e67e52528a
sabamiso11 <adachi.business.11@gmail.com>
2018-06-29 13:47:07 +0900

    @user = User.find[:user_id]をbefore_action化

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -1,33 +1,28 @@
 class InterviewsController < ApplicationController
+  before_action :find_user
   def index
-    @user = User.find(params[:user_id])
     @interviews = @user.interviews.all.order(start_interview: "ASC")
     @interview = @user.interviews.new
   end
 
   def new
-    @user = User.find(params[:user_id])
     @interview = @user.interviews.new
   end
 
   def create
-    @user = User.find(params[:user_id])
     @interview = @user.interviews.new(start_interview_params)
     if @interview.save
       redirect_to :action => "index"
     else
-      #render plain: @interview.errors.inspect
       render 'new'
     end
   end
 
   def edit
-    @user = User.find(params[:user_id])
     @interview = @user.interviews.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:user_id])
     @interview = @user.interviews.find(params[:id])
     if @interview.update(start_interview_params)
       redirect_to :action => "index"
@@ -37,14 +32,12 @@ class InterviewsController < ApplicationController
   end
 
   def destroy
-    @user = User.find(params[:user_id])
     @interview = @user.interviews.find(params[:id])
     @interview.destroy
     redirect_to :action => "index"
   end
 
   def state
-    @user = User.find(params[:user_id])
     @interviews = @user.interviews.all.order(start_interview: "ASC")
     @interview = @user.interviews.find(params[:id])
     @interview.state = 1
@@ -56,8 +49,6 @@ class InterviewsController < ApplicationController
       flash[:notice_interview] = "面接日程の承認通知を送信しました"
       redirect_to :action => "index"
     else
-      #render plain: @interviews.errors.inspect
-      #render :file => "_error.html.erb"
       render 'index'
     end
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :sign_in_required
+  before_action :find_user, only: :desired
   def index
     @users = User.select("users.*, interviews.start_interview")
       .joins("LEFT OUTER JOIN interviews ON users.id = interviews.user_id AND interviews.state = 1")
@@ -8,10 +9,10 @@ class UsersController < ApplicationController
   end
 
   def desired
-    @user = User.find(params[:user_id])
     NotificationMailer.interviewer_desired(@user, current_user).deliver_later
     NotificationMailer.examinee_desired(current_user).deliver_later
     flash[:notice_interview] = "面接希望申請を送信しました"
     redirect_to :action => "index"
   end
+
 end
